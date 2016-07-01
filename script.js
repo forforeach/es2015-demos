ef50e9ced1d07a39382f755100f9d78f988364d4
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-07-01 23:18:18 +0300

    object function start

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function createContact(firstName, lastName) {
     let fullName = firstName + ' ' + lastName;
-    return { firstName, lastName, fullName };
+    return {
+        firstName,
+        lastName,
+        printFullName: function () {
+            console.log(fullName);
+        }
+    };
 }
 
-let {firstName, lastName, fullName}
-    = createContact('Dima', 'Kuzmich');
-
-console.log(firstName);
-console.log(lastName);
-console.log(fullName);
+let contact = createContact('Dima', 'Kuzmich');
+contact.printFullName();
