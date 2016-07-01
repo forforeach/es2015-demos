07694201811d863919a1ce556f62d18976aad705
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-07-01 21:29:30 +0300

    default dialog options

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-function Dialog(title, options) {
+function Dialog(title, options = {}) {
     this.title = title;
     this.width = options.width;
     this.height = options.height;
     this.draggable = options.draggable;
 }
 
-var dialog = new Dialog('Hello', {
+var dialog = new Dialog('Dialog', {
     width: 100,
     height: 200,
     draggable: false
 });
 
-var dialogDefault = new Dialog('Hello');
+console.log('dialog',
+    JSON.stringify(dialog, null, 2));
+
+var dialogDefault = new Dialog('Default Dialog');
+
+console.log('dialogDefault',
+    JSON.stringify(dialogDefault, null, 2));
