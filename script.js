e4655f8156c77503bb2e7effd26144f2be18685e
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-07-01 22:53:05 +0300

    arrow functions end

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ function TagsVM(entity, record) {
 }
 
 TagsVM.prototype.loadTags = function (...tags) {
-    setTimeout(function () {
+    setTimeout(() => {
         this.tags = ['async', 'tags', 'grabbed'];
+        console.log(window.tagsVM.tags);
     });
 }
 var tagsVM = new TagsVM(1, 1);
 tagsVM.loadTags();
-
-console.log(tagsVM.tags);
