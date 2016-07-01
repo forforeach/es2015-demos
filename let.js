899d1cebbe1b2bf8aa452e3316a624fc811ce852
unknown <Dima.Kuzmich@dimaku-lap.clickdimensionsdev.com>
2016-07-01 20:10:54 +0300

    for loop

--- let.js
+++ let.js
@@ -1,16 +1,16 @@
-// The hoisting
+// async loop
 
-function greet(name) {
-
-    if (name) {
-        let message = 'Hello, ' + name;
-        console.log(message);
-    } else {
-        let warning = 'To get a greeing you should say your name';
-        console.warn(warning);
-    }
-    console.log('message is', message);
-    console.log('warning is', warning);
+function async(param, callback) {
+    setTimeout(callback, 100);
 }
 
-greet('everyone');
+function proceedAsync(params) {
+    // foreach loop
+    for (var i = 0; i < params.length; i++) {
+        async(params[i], function () {
+            console.log('response for ' + params[i]);
+        });
+    }
+}
+
+proceedAsync(['first', 'second', 'third']);
