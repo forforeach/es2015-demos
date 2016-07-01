69de667b0b41e56e2caa869d4bd8b91246f71d7b
unknown <Dima.Kuzmich@dimaku-lap.clickdimensionsdev.com>
2016-07-01 19:58:41 +0300

    let end

--- let.js
+++ let.js
@@ -1,11 +1,12 @@
 // The hoisting
 
 function greet(name) {
+
     if (name) {
-        var message = 'Hello, ' + name;
+        let message = 'Hello, ' + name;
         console.log(message);
     } else {
-        var warning = 'To get a greeing you should say your name';
+        let warning = 'To get a greeing you should say your name';
         console.warn(warning);
     }
     console.log('message is', message);
