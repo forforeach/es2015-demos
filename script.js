acf2d202e7d02ed1f5291cf8f2c002cab843ffff
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-07-01 21:49:02 +0300

    rest param end

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ function addTag(tag) {
     console.log(tag + ' was added');
 }
 
-function addTags() {
-    var args = Array.prototype.slice.call(arguments);
-
-    args.forEach(function (tag) {
+function addTags(...tags) {
+    tags.forEach(function (tag) {
         addTag(tag);
     });
 }
