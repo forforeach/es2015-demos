d2fd9db8eea07b3190bf6f49da33cf3740168885
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-07-01 21:26:23 +0300

    named params start

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function printArray(array = []) {
-    array.forEach(function (element) {
-        console.log(element);
-    });
+function Dialog(title, options) {
+    this.title = title;
+    this.width = options.width;
+    this.height = options.height;
+    this.draggable = options.draggable;
 }
 
-printArray([1, 2, 3, 4, 5]);
-printArray();
+var dialog = new Dialog('Hello', {
+    width: 100,
+    height: 200,
+    draggable: false
+});
+
+var dialogDefault = new Dialog('Hello');
