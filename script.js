635559b83a0761df200078debab83319b805cf51
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-07-02 18:14:41 +0300

    1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let firstUser = { name: 'Dima', isActive: false }
 let secondUser = { name: 'Trololosha', isActive: true };
 
-let totalLogins = {};
-totalLogins[firstUser] = 5;
-totalLogins[secondUser] = 60;
+let totalLogins = new Map();
+totalLogins.set(firstUser, 5);
+totalLogins.set(secondUser, 60);
 
-console.log(JSON.stringify(totalLogins, null, 2));
+console.log(totalLogins);
