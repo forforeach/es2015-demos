c1f7a359cc78cfa67f1da851c50e1cc9c8255439
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-07-02 17:55:19 +0300

    1

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 let tags = ['async', 'tags', 'grabbed'];
 
-let [a, b, c] = tags;
-console.log(a, b, c);
-
-let [x, , y] = tags;
-console.log(x, y);
-
-let [first, ...rest] = tags;
-console.log(first, rest);
+tags.forEach(function (element) {
+    console.log(element);
+});
