ebb76123fa833c0f9bf979a61224c0408a617f3a
Dima Kuzmich <Dima.Kuzmich@clickdimensionsdev.com>
2016-07-11 14:48:39 +0300

    3

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ function TagsVM(entity, record) {
 TagsVM.prototype.loadTags = function (...tags) {
     setTimeout(function () {
         this.tags = ['async', 'tags', 'grabbed'];
-        console.log(window.tagsVM.tags);
-    });
+    }, 1000);
 }
 var tagsVM = new TagsVM(1, 1);
 tagsVM.loadTags();
+
+
+setTimeout(function () {
+    console.log(tagsVM.tags);
+}, 1500);
